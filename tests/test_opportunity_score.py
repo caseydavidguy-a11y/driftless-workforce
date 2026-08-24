f3bd5dca3bf10cc5cdd7eefdd157514e3006973b
caseydavidguy-a11y <caseydavidguy@gmail.com>
2026-08-24 04:18:11 -0500

    Correct opportunity score test to match configured momentum cap

--- tests/test_opportunity_score.py
+++ tests/test_opportunity_score.py
@@ -7,7 +7,7 @@ class OpportunityScoreTests(unittest.TestCase):
         signals=[HiringSignal('leadership_new','high','New supervisor opening','Supervisor')]
         momentum={'direction':'up','pct':100,'change':5}
         score=calculate_opportunity_score(55,signals,momentum)
-        self.assertEqual(score,77)
+        self.assertEqual(score,82)
         self.assertEqual(priority_for_score(score),'Pursue')
 
     def test_score_is_bounded(self):
